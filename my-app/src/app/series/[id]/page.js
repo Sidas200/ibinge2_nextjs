bc2392ad179595a93d9b8ee191a14d997748c008
rexxrm <reginarmsj@gmail.com>
2024-11-13 09:31:18 -0600

    perrahambreada branch

---
+++ my-app/src/app/series/[id]/page.js
@@ -0,0 +1,19 @@
+// src/app/series/[id]/page.js
+import SeriesDetails from '../../componentes/SeriesDetails';
+import { fetchShowDetails, fetchCast } from '../../lib/mazeApi';
+
+async function SeriesPage({ params }) {
+  const { id } = params;
+
+  // Fetch show details and cast data
+  const showDetails = await fetchShowDetails(id);
+  const castDetails = await fetchCast(id);
+
+  return (
+    <div>
+      <SeriesDetails showDetails={showDetails} castDetails={castDetails} />
+    </div>
+  );
+}
+
+export default SeriesPage;
