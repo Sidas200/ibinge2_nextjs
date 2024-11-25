24afe98d3d61fd02f4ee8dd422fddbade1f2d9d3
rexxrm <reginarmsj@gmail.com>
2024-11-24 23:02:43 -0600

    navbarsilla

--- my-app/src/app/page.js
+++ my-app/src/app/page.js
@@ -75,9 +75,12 @@ export default function Home() {
 
     return (
         <div style={{ overflow: "hidden" }}>
-            <div className="ibinge">
-                <Typography sx={{ color: "white", fontSize: "100px" }}>iBinge</Typography>
+            <div className="fondito">
+                <div className="ibinge">
+                    <Typography sx={{ color: "white", fontSize: "100px" }}>iBinge</Typography>
+                </div>
             </div>
+
             <hr />
             {showNavBar && <NavBar onSearch={handleSearch} />}
             {loading ? (
